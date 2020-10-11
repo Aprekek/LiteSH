2ba1e423acabb9391783bc2ea58eed0f54381227
daniil <daniillepekhin@yandex.ru>
2020-10-11 13:06:00 +0700

    Change arguments

--- src/help.cpp
+++ src/help.cpp
@@ -17,23 +17,22 @@ void printArguments()
 {
   cout << "--help"<<endl;
 	cout << "-h"<<endl;
-	cout << "-m - Move files"<<endl;
-	cout << "-c - Copying file"<<endl;
-	cout << "-d - File deletion"<<endl;
-	cout << "-s - Calculating the total size of the specified directory or file"<<endl;
-	cout << "-af - Displaying all files in the specified directory"<<endl;
-	cout << "-ap - Displaying all processes from the procfs file system"<<endl;
+	cout << "-c - Calling a line for entering project call commands"<<endl;
+	cout << "-p - A product of the process by its name"<<endl;
+	cout << "-cmd - Return to the command line"<<endl;
+	cout << "-bm - Putting the process in the background mode"<<endl;
+	cout << "-expr - Signals from external progmans"<<endl;
+	cout << "-os - Signals from the OS"<<endl;
 }
 
 void printReadme()
 {
     cout << "This program performs the following functions:"<<endl<< 
-    "*copying a file;"<<endl<<  
-    "*moving a file;" <<endl<<  
-    "*deleting a file;"<<endl<<
-    "*calculating the total size of a directory or file; "<<endl<<
-    "*displaying all file system processes;" <<endl<<
-    "*displaying all files in a directory."<<endl<<
+    "*calling a line for entering project call commands;"<<endl<<  
+    "*a product of the process by its name;" <<endl<<  
+    "*return to the command line;"<<endl<<
+    "*putting the process in the background mode; "<<endl<<
+    "*receive and process signals from external programs and the operating system;" <<endl<<
     "Check the appropriate arguments before starting!"<<endl;
 }
 
