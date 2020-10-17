f873256d048a2ddaa3c65d2a45772c70a2777d37
OwlinOhana <47669603+OwlinOhana@users.noreply.github.com>
2020-10-17 15:59:58 +0700

    Update Clieserv.c

--- src/Clieserv.c
+++ src/Clieserv.c
@@ -1,7 +1,5 @@
 #include "Clieserv.h"
 
-clients *Clients[MAX_CLIENT];
-
 
 int Socket(int domain, int type, int protocol)
 {
@@ -83,22 +81,6 @@ int Write(int handle, void *buf, int count)
     return res;
 }
 
-int Sendto(int sockfd, const void *buf, size_t len, int flags, struct sockaddr *addr, socklen_t addrlen)
-{
-    int res = sendto(sockfd, buf, len, flags, addr, addrlen);
-    if (res < 0)
-        perror("Sendto failed\n");
-    return res;
-}
-
-int Recv(int sockfd, void *buf, size_t len, int flags)
-{
-    int res = recv(sockfd, buf, len, flags);
-    if (res < 0)
-        perror("resvfrom failed\n");
-    return res;
-}
-
 void printIpAddr(struct sockaddr_in addr)
 {
     printf("%d.%d.%d.%d", 
@@ -107,81 +89,3 @@ void printIpAddr(struct sockaddr_in addr)
             (addr.sin_addr.s_addr & 0xff0000) >> 16,
             (addr.sin_addr.s_addr & 0xff000000) >> 24);
 }
-
-void str_trim_lf (char* arr, int length) 
-{
-  for (int i = 0; i < length; i++)
-    if (arr[i] == '\n') 
-    {
-      arr[i] = '\0';
-      break;
-    }
-  
-}
-
-// void *handleClient(void *arg)
-// {
-//     char buf[Buffer_size];
-//     char name[NAME_LEN];
-//     int leaveFlags;
-
-//     cliCount++;
-//     clients *cli = (clients *)arg;
-
-//     if(recv(cli->sockfd, name, NAME_LEN, 0) <= 0 || strlen(name) <  2 || strlen(name) >= NAME_LEN-1)
-//     {
-// 		printf("Didn't enter the name.\n");
-// 		leaveFlags = 1;
-// 	} 
-//     else
-//     {
-// 		strcpy(cli->name, name);
-// 		sprintf(buf, "%s has joined\n", cli->name);
-// 		printf("%s", buf);
-// 		//sendMessage(buf, cli->uid);
-// 	}
-
-// 	bzero(buf, Buffer_size);
-
-//     while(1)
-//     {
-// 	 if (leaveFlags)
-//           break;
-// 	else if (strcmp(buf, "exit") == 0)
-//         {
-//             sprintf(buf, "%s has left\n", cli->name);
-//             printf("%s", buf);
-//             //sendMessage(buf, cli->uid);
-//             leaveFlags = 1;
-//         }
-      
-// 	else if ( fork() )
-// 	   close(cli->uid);
-// 	   //FIXME
-// 	else if
-// 	{
-// 		close(sd); /* клиенту не нужен доступ к сокету */
-// 		dup2(cli->uid, 0); /* замещаем поток stdin */ //cli->name(?)
-// 		dup2(cli->uid, 1); /* замещаем поток stdout */
-// 		dup2(cli->uid, 2); /* замещаем поток stderr */
-// 		execl("/", "/", 0);
-// 		perror("Exec failed!"); /* что-то случилось */
-// 	}
-	
-// 	else 
-//         {
-//             printf("-1\n");
-//             leaveFlags = 1;
-//         }
-        
-//     }
-
-//     close(cli->sockfd);
-//     QueueRemove(cli->uid);
-//     free(cli);
-//     cliCount--;
-//     pthread_detach(pthread_self());
-
-//     return 0;
-
-// }
