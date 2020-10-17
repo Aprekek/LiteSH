734b5a6f5a0b31b3178e68763c3d342203d85291
OwlinOhana <47669603+OwlinOhana@users.noreply.github.com>
2020-10-17 21:44:36 +0700

    Update Clieserv.h

--- src/Clieserv.h
+++ src/Clieserv.h
@@ -12,18 +12,8 @@
 #include <arpa/inet.h>
 #include <string.h>
 //#include <signal.h>
-
-#define Buffer_size 2048
-
-typedef struct 
-{
-    struct sockaddr_in addres;
-    int sockfd;
-    int uid;
-    char name [NAME_LEN];
-} clients;
-
-
+#define MAX_MSG_LENGTH 1024
+pid_t fork(void);
 
 static int cliCount = 0;
 static int uid = 10;
@@ -33,8 +23,8 @@ void Bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
 void Listen(int sockfd, int backlog);
 int Accept(int sickfd, struct sockaddr *addr, socklen_t *addrlen);
 void Connect(int socket, const struct sockaddr *addr, socklen_t addrlen);
-void Inet_pton(int af, const char *src, void *dst);
-void printIpAddr(struct sockaddr_in addr);
+//void Inet_pton(int af, const char *src, void *dst);
+//void printIpAddr(struct sockaddr_in addr);
 
 
 #endif
