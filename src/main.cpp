fe2572504250b803024bdc279b9d354adb7dfb65
DaniilGit <daniil.work2019@gmail.com>
2020-10-18 17:00:57 +0700

    Change

--- src/main.cpp
+++ src/main.cpp
@@ -4,41 +4,31 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    string str, strHelp, strH;
+    string str, strHelp, strH, strLab2, strProc, strProcBg, strSignal;
     strHelp = "--help";
     strH = "-h";
+    strLab2 = "-l2";
+    strProc = "-p";
+    strProcBg = "-pb";
+    strSignal = "-signal";
 
     if (argc == 2) {
         if (argv[1] == strHelp || argv[1] == strH) // Help
             Help();
-        else {
+        else if (argv[1] == strLab2) {
+            if (launchLab2() != 0)
+                cout << "Error" << endl;
+        } else if (argv[1] == strProc) {
+            if (spawnProcess() != 0)
+                cout << "Error" << endl;
+        } else if (argv[1] == strProcBg) {
+            if (spawnProcessFone() != 0)
+                cout << "Error" << endl;
+        } else if (argv[1] == strSignal) {
+            catchSignal();   
+        } else {
             cout << "Incorrect argument" << endl;
             return 0;
-        }
-    } else if (argc == 1) {
-        while (1) {
-            cout << "===========================" << endl;
-            cout << "Enter the required action: " << endl;
-            cout << "'-l2' - Command line from lab 2" << endl;
-            cout << "'-p' - Spawn a process by name" << endl;
-            cout << "'-pb' - Put the process in the background" << endl;
-            cout << "'-signal' - Processing of signals" << endl;
-            cout << "===========================" << endl;
-            cin >> str;
-            if (str == "-l2") {
-                if (launchLab2() != 0)
-                    cout << "Error" << endl;
-            } else if (str == "-p") {
-                if (spawnProcess() != 0)
-                    cout << "Error" << endl;
-            } else if (str == "-pb") {
-                if (spawnProcessFone() != 0)
-                    cout << "Error" << endl;
-            } else if (str == "-signal") {
-                catchSignal();    
-            } else {
-                cout << "Incorrect argument" << endl;
-            }
         }
     } else {
         cout << "Incorrect argument" << endl;
