670dc49ecc260607a0f6c1d32b41692b793fe02d
OwlinOhana <47669603+OwlinOhana@users.noreply.github.com>
2020-10-19 18:55:06 +0700

    add pthread

--- src/server.c
+++ src/server.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
     //listenfd = serverAdr
     struct sockaddr_in serverAdr = {0};
     struct sockaddr_in clientAddr = {0};
-   
+    pthread_t tid;
 
     int server = Socket(AF_INET, SOCK_STREAM, 0); //протокол
     serverAdr.sin_family = AF_INET;
@@ -26,27 +26,19 @@ int main(int argc, char *argv[])
 
     Bind(server, (struct sockaddr *) &serverAdr, sizeof serverAdr);
 
-    Listen(server, 5);
-
-    socklen_t clieLen = sizeof(clientAddr);
-    connfd = Accept(server, (struct sockaddr *) &clientAddr, &clieLen);
-
-    char buf[1024];
-		
-    socklen_t clieLen = sizeof(clientAddr);
-    connfd = Accept(server, (struct sockaddr *) &clientAddr, &clieLen);
-		  
+    Listen(server, 3);
+	
     dup2(connfd,1);    //redirect stdout
      while(1)
      {
-	recv(connfd,buf, MAX_MSG_LENGTH,0);
-	/*If the message recieved was END_STRING, exit this loop*/
-	system(buf);	
-	bzero(buf, MAX_MSG_LENGTH);
+		socklen_t clieLen = sizeof(clientAddr);
+        connfd = Accept(server, (struct sockaddr *) &clientAddr, &clieLen);
+        clients *cli = (clients *)malloc(sizeof(clients));
+        cli->addres = clientAddr;
+        cli->sockfd = connfd;
+        cli->uid = uid++;
+		Pthread_create(&tid, NULL, &handleClient, (void *)cli);
      }	
     
-    close(connfd);
-    sleep(1);
-    close(server);
     return 0;
 }
