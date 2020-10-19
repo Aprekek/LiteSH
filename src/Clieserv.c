95f360d164bf3a6c2af2f1cdd7166e358ba9a6dd
OwlinOhana <47669603+OwlinOhana@users.noreply.github.com>
2020-10-19 19:09:59 +0700

    add pthread

--- src/Clieserv.c
+++ src/Clieserv.c
@@ -89,3 +89,59 @@ void printIpAddr(struct sockaddr_in addr)
             (addr.sin_addr.s_addr & 0xff0000) >> 16,
             (addr.sin_addr.s_addr & 0xff000000) >> 24);
 }
+
+int Pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arq)
+{
+    int res = pthread_create(thread, attr, start_routine, arq);
+    if (res != 0)
+    {
+        perror("pthread_create failed\n");
+        exit(1);
+    }
+    return res;
+}
+
+
+void *handleClient(void *arg)
+{  
+    char str[10], strHelp[10], strH[10], strLab2[10], strProc[10], strProcBg[10], strSignal[10];
+    strHelp = "--help";
+    strH = "-h";
+    strLab2 = "-l2";
+    strProc = "-p";
+    strProcBg = "-pb";
+    strSignal = "-signal";
+ 
+    char buf[MAX_MSG_LENGTH];
+    while(1)
+    {
+        recv(connfd,buf, MAX_MSG_LENGTH,0);
+        if (buf == strHelp || buf == strH) // Help
+            Help();
+        else if (buf == strLab2) 
+            if (launchLab2() != 0)
+                printf("Error\n");
+         else if (buf == strProc) 
+            if (spawnProcess() != 0)
+                 printf("Error\n");
+         else if (buf == strProcBg) 
+            if (spawnProcessFone() != 0)
+                printf("Error\n");
+         else if (buf == strSignal) 
+            catchSignal();   
+         else 
+         {
+            printf("Error\n");
+            return 0;
+         }
+      
+        bzero(buf, MAX_MSG_LENGTH);
+    }
+
+    close(cli->sockfd);
+    free(cli);
+    pthread_detach(pthread_self());
+
+    return 0;
+
+}
