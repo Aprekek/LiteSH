cb851ee1b33e687d727cbaa18dbb04745bbfc072
OwlinOhana <47669603+OwlinOhana@users.noreply.github.com>
2020-11-05 17:19:44 +0700

    Beautified C Code

--- src/client.c
+++ src/client.c
@@ -1,16 +1,16 @@
 #include "Clieserv.h"
 #include "filesystem.h"
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-    if (argc != 2) 
-	{
+    if (argc != 2)
+    {
         perror("ERROR, no port provided\n");
         exit(1);
     }
-    
-	char *ip = "127.0.0.1";
-	int port = atoi(argv[1]);
+
+    char *ip = "127.0.0.1";
+    int port = atoi(argv[1]);
     struct sockaddr_in server = {0};
 
     int sockfd = Socket(AF_INET, SOCK_STREAM, 0); //протокол
@@ -21,61 +21,61 @@ int main(int argc, char *argv[])
     // Connect to Server
     Connect(sockfd, (struct sockaddr *)&server, sizeof server);
 
-	int flag = 0;
-	while(flag ==0)
-	{
-		char *sendBuf = (char *) calloc(SizeBufSend, sizeof (char)); 
-		char *recvBuf = (char *) calloc (SizeBufRecv, sizeof (char));
- 		printf("\n**********Welcome**********\n");
-
-		printf("Enter operation and param\n");
-		fgets(sendBuf, SizeBufSend, stdin);
-
-
-		if (strcmp(sendBuf, "exit") == 0)
-			flag = 1;
-
-		if(write (sockfd, sendBuf, strlen(sendBuf)) < 1)
-		{
-			perror ("ERROR, message can't sended");
-			break;
-		}
-
-		if (read(sockfd, recvBuf,SizeBufRecv) == -1)
-		{
-			perror ("ERROR, message can't recieved");
-			break;
-		}
-		
-		else 
-			puts(recvBuf);
-
-		free(sendBuf);
-		free(recvBuf);
-	}
-	
     
+    while(1)
+    {
+        char *sendBuf = (char *) calloc(SizeBufSend, sizeof (char));
+        char *recvBuf = (char *) calloc (SizeBufRecv, sizeof (char));
+        printf("\n**********Welcome**********\n");
+
+        printf("Enter operation and param\n");
+        fgets(sendBuf, SizeBufSend, stdin);
+
+
+        if (strcmp(sendBuf, "exit") == 0)
+            return 0;
+
+        if(write (sockfd, sendBuf, strlen(sendBuf)) < 1)
+        {
+            perror ("ERROR, message can't sended");
+            break;
+        }
+
+        if (read(sockfd, recvBuf,SizeBufRecv) == -1)
+        {
+            perror ("ERROR, message can't recieved");
+            break;
+        }
+
+        else
+            puts(recvBuf);
+
+        free(sendBuf);
+        free(recvBuf);
+    }
+
+
     // int read_size;
     // char buf[256];
-	
-/*	while(fgets(buf,MAX_MSG_LENGTH,stdin))
-	{
-		send(sockfd,buf,MAX_MSG_LENGTH,0);
-		
-		while(1)
-		{
-		    read_size = read(sockfd,buf,MAX_MSG_LENGTH);
-		    //Print read_size characters from buf
-		    printf("%.*s\n", read_size, buf);
-		    if (read_size < MAX_MSG_LENGTH)
-		        //Keep on reading until read_size < MAX_MSG_LENGTH, then exit this loop
-		        break;
-		    
-		}
-		
-	    bzero(buf, MAX_MSG_LENGTH);
-	}	
-*/	
-	close(sockfd);
-	return 0;
+
+    /*	while(fgets(buf,MAX_MSG_LENGTH,stdin))
+    	{
+    		send(sockfd,buf,MAX_MSG_LENGTH,0);
+
+    		while(1)
+    		{
+    		    read_size = read(sockfd,buf,MAX_MSG_LENGTH);
+    		    //Print read_size characters from buf
+    		    printf("%.*s\n", read_size, buf);
+    		    if (read_size < MAX_MSG_LENGTH)
+    		        //Keep on reading until read_size < MAX_MSG_LENGTH, then exit this loop
+    		        break;
+
+    		}
+
+    	    bzero(buf, MAX_MSG_LENGTH);
+    	}
+    */
+    close(sockfd);
+    return 0;
 }
