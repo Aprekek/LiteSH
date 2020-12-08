c779cc0084eb29ab32f230daf3fc0c7459eeab92
DaniilGit <axsell232@gmail.com>
2020-12-08 17:11:03 +0700

    Tests 1

--- test/test.c
+++ test/test.c
@@ -4,14 +4,54 @@
 
 CTEST(filesystem, getFileSize)
 {
+    int expected = 69;
+    int result = getFileSize("./test/folder_test/test.txt");
+
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(filesystem, getDirSize)
+{
+    int expected = 54 + 69;
+    int result = getDirSize("./test/folder_test");
+
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(filesystem, displayAllFiles)
+{
     int expected = 0;
+    int result = displayAllFiles("./test/folder_test");
 
-    FILE* test;
-    test = fopen("test.txt", "w");
-    int result = getFileSize("test.txt");
+    ASSERT_EQUAL(expected, result);
+}
 
-    fclose(test);
-    remove("rest.txt");
+CTEST(filesystem, deleteFile)
+{
+    FILE *test;
+    test = fopen("test3.txt", "w");
+    
+    int expected = -1;
+    int result = deleteFile("test3.txt");
+
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(filesystem, copyFile)
+{
+    int expected = 0;
+    int result = copyFile("./test/folder_test/folder_test2", "test4.txt");
+
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(filesystem, moveFile)
+{
+    FILE *test;
+    test = fopen("test5.txt", "w");
+
+    int expected = -1;
+    int result = moveFile("./test/folder_test/folder_test2", "test5.txt");
 
     ASSERT_EQUAL(expected, result);
 }
