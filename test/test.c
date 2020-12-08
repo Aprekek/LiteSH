f78145cac36464321d02b6b69c42186476603c6e
DaniilGit <axsell232@gmail.com>
2020-12-08 14:35:53 +0700

    CI

--- test/test.c
+++ test/test.c
@@ -1,10 +1,17 @@
 #include "../thirdparty/ctest.h"
 #include "filesystem.h"
+#include <cstdio>
 
 CTEST(filesystem, getFileSize)
 {
-    int expected = 3432;
-    int result = getFileSize("./main.c");
+    int expected = 0;
+
+    FILE* test;
+    test = fopen("test.txt", "w");
+    int result = getFileSize("test.txt");
+
+    fclose(test);
+    remove("rest.txt");
 
     ASSERT_EQUAL(expected, result);
 }
